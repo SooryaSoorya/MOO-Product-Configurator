d15c9f3e7c8953eaf65b41b9774733a9718edf71
Soorya S <haisoorya.s@gmail.com>
2024-08-20 05:13:39 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import ProductConfigurator from "./components/ProductConfigurator";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main>
+        <ProductConfigurator />
+      </main>
+    </div>
+  );
+}
